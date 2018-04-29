64e8e0201b0a44bf024d4f7f369a75b008d7a4a2
lizheming <secretlzm007@gmail.com>
2018-04-29 17:46:20 +0800

    增加退出登录交互

--- www/static/src/home/components/Header.jsx
+++ www/static/src/home/components/Header.jsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import {Link} from 'react-router-dom';
-import {Layout, Menu} from 'antd';
+import {Layout, Menu, Dropdown, Avatar, Icon} from 'antd';
+
+import rq from './request';
 
 const navs = [
   {
@@ -12,8 +15,23 @@ const navs = [
   }
 ];
 
+async function logout() {
+  await rq.delete('/api/token');
+  location.reload();
+}
+
 export default props => {
   const defaultSelectedKeys = [location.pathname];
+  if (!global.userInfo.name) {
+    return (
+      <Layout.Header>
+        <div className="logo">
+          <Link to={{pathname: '/'}}>Animaris</Link>
+        </div>
+      </Layout.Header>
+    );
+  }
+
   return (
     <Layout.Header>
       <div className="logo">
@@ -31,6 +49,22 @@ export default props => {
           </Menu.Item>
         )}
       </Menu>
+      <div class="user-pop">
+        <Dropdown overlay={<Menu>
+          <Menu.Item disabled>
+            <Icon type="user" />个人中心
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item key="logout">
+            <span onClick={logout}><Icon type="logout" />退出登录</span>
+          </Menu.Item>
+        </Menu>}>
+          <span>
+            <Avatar size="small" src="https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png" />
+            <span>{global.userInfo.name}</span>
+          </span>
+        </Dropdown>
+      </div>
     </Layout.Header>
   );
 };
